bdf0f4b9367945ad1bfc4c7e0a7a989ed3d264db
AnisurRahman06046 <anisurrahman06046@gmail.com>
2022-11-23 00:19:01 +0600

    fixed

--- index.js
+++ index.js
@@ -50,6 +50,9 @@ async function run() {
     const bookingCollection = client.db("doctorsPortal").collection("bookings");
     const usersCollection = client.db("doctorsPortal").collection("users");
     const doctorssCollection = client.db("doctorsPortal").collection("doctors");
+    const paymentsCollection = client
+      .db("doctorsPortal")
+      .collection("payments");
 
     const verifyAdmin = async (req, res, next) => {
       const decodedEmail = req.decoded.email;
@@ -135,6 +138,26 @@ async function run() {
       res.send({
         clientSecret: paymentIntent.client_secret,
       });
+    });
+
+    app.post("/payments", async (req, res) => {
+      const payment = req.body;
+      const result = await paymentsCollection.insertOne(payment);
+      const id = payment.bookingId;
+      const query = {
+        _id: ObjectID(id),
+      };
+      const updatedDoc = {
+        $set: {
+          paid: true,
+          transactionId: payment.transactionId,
+        },
+      };
+      const updatedResult = await bookingCollection.updateOne(
+        query,
+        updatedDoc
+      );
+      res.send(result);
     });
 
     app.get("/jwt", async (req, res) => {
